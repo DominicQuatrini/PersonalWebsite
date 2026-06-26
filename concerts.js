e0a7a47015c0da25b39a777aea493d772b65c132
Dominic Quatrini <dominicquatrini@gmail.com>
2026-06-26 14:55:01 -0700

    working leaflet map

---
+++ concerts.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([47.6062, -122.3321], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
