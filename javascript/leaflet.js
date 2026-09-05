7846ec1bc4138f01fa8252ad8d47a4bc176d773d
Dominic Quatrini <dominicquatrini@gmail.com>
2026-09-05 16:39:48 -0700

    more leaflet bugfixes

--- javascript/leaflet.js
+++ javascript/leaflet.js
@@ -7,48 +7,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let climatePledgeArenaLat = 47.622238569760036;
-let climatePledgeArenaLng = -122.35393044884134;
-
-let tMobileParkLat = 47.591728807371010;
-let tMobileParkLng = -122.33254027298267;
-
-let madameLousLat = 47.614626832629284;
-let madameLousLng = -122.34911857005739;
-
-let edgefieldManorLat = 45.537422578462255;
-let edgefieldManorLng = -122.40711052464210;
-
-let dolbyLiveLat = 36.104155412313524;
-let dolbyLiveLng = -115.17503132353661;
-
-let wamuLat = 47.59381721988933;
-let wamuLng = -122.332380737562;
-
-let showboxSodoLat = 47.58805586456722;
-let showboxSodoLng = -122.33390097738757;
-
-let paramountTheatreLat = 47.613813644437194;
-let paramountTheatreLng = -122.33140609789473;
-
-let mooreTheatreLat = 47.61190933244898;
-let mooreTheatreLng = -122.34139279260559;
-
-let modaCenterLat = 45.531722872505654;
-let modaCenterLng = -122.66684229286913;
-
-let crystalBallroomLat = 45.52315175071015;
-let crystalBallroomLng = -122.68484107121779;
-
-let neptuneTheatreLat = 47.66153344483183;
-let neptuneTheatreLng = -122.31404495694926;
-
-let crocodileLat = 47.614970147788604;
-let crocodileLng = -122.3491493445646;
-
-let gorgeAmphitheatreLat = 47.09992492081298;
-let gorgeAmphitheatreLng = -119.99469097865824;
-
 /*
 const concerts = [
     new concert("Cigarettes After Sex", "McMenamins Historic Edgefield Manor", "North American Tour 2023", "August 26, 2023", "Troutdale", "OR", 31.20, edgefieldManorLat, edgefieldManorLng),
