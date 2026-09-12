f9e10b5a187ca14bf8fad9a4f0d468359d381eab
Dominic Quatrini <dominicquatrini@gmail.com>
2026-09-12 12:25:25 -0700

    group concat and style changes

--- functions/api/concerts.js
+++ functions/api/concerts.js
@@ -3,7 +3,7 @@ export async function onRequest(context) { // server-side API endpoint and Cloud
         SELECT
             c.concert_id, c.concert_date, c.ticket_price, c.tour_name,
             v.venue_id, v.venue_name, v.venue_latitude, v.venue_longitude,
-            GROUP_CONCAT(a.artist_name) AS artists -- combines opener(s) and headliner into single string
+            GROUP_CONCAT(a.artist_name, ', ') AS artists -- combines opener(s) and headliner into single string
         FROM concerts c
         JOIN venues v ON c.venue_id = v.venue_id
         JOIN concert_artists ca ON c.concert_id = ca.concert_id
