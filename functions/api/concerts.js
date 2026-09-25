f06d77cfefd74b2467d18536642f2f7868591c9a
Dominic Quatrini <dominicquatrini@gmail.com>
2026-09-24 23:37:59 -0700

    final bugfix i hope

--- functions/api/concerts.js
+++ functions/api/concerts.js
@@ -5,13 +5,10 @@ export async function onRequest(context) { // server-side API endpoint and Cloud
             v.venue_id, v.venue_name, v.venue_latitude, v.venue_longitude,
             (
                 SELECT GROUP_CONCAT(a.artist_name, ', ')
-                FROM (
-                    SELECT a.artist_name
-                    FROM concert_artists ca
-                    JOIN artists a ON ca.artist_id = a.artist_id
-                    WHERE ca.concert_id = c.concert_id
-                    ORDER BY CASE WHEN ca.artist_role = 'headliner' THEN 0 ELSE 1 END
-                )
+                FROM concert_artists ca
+                JOIN artists a ON ca.artist_id = a.artist_id
+                WHERE ca.concert_id = c.concert_id
+                ORDER BY ca.artist_role ASC LIMIT -1
             ) AS artists
         FROM concerts c
         JOIN venues v ON c.venue_id = v.venue_id
